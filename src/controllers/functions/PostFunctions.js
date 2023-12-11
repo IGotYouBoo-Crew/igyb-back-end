7120780b32205f9bad349d504c2ae92e559f1cfb
JackDixon019 <13709@coderacademy.edu.au>
2023-12-11 13:33:38 +1100

    Feat: added basic routes for post CREATE, UPDATE, and DELETE

--- src/controllers/functions/PostFunctions.js
+++ src/controllers/functions/PostFunctions.js
@@ -2,18 +2,44 @@
 // create functionality involving them.
 const { Post } = require('../../models/PostModel');
 
+// CREATE
+async function createNewUser(data){
+    return await Post.create(data).catch((error) => error)
+}
+
+// READ
 // Model.find({}) returns all documents in a collection.
 async function getAllPosts(){
-    return await Post.find({}).populate("author", "username");
+    return await Post.find({}).populate("role");
+    
+}
+
+async function getPostByTitle(postTitle){
+    // finds one post with matching postTitle
+    return await Post.findOne({title: postTitle}).exec()
 }
 
 async function getPostById(postId){
-    // finds one user with matching username
-    return await Post.findOne({id: postId}).exec()
+    // finds post with matching postId
+    return await Post.findById(postId).exec()
+}
+
+// UPDATE
+async function updatePostById(postId, updatedPostData){
+    return await Post.findByIdAndUpdate(postId, updatedPostData, { runValidators: true, returnDocument: 'after' }).exec()
+}
+
+// DELETE
+async function deletePostById(postId){
+    return await Post.findByIdAndDelete(postId).exec()
 }
 
 // Export the functions for our routes to use.
 module.exports = {
     getAllPosts,
-    getPostById
+    getPostByTitle,
+    getPostById,
+    deletePostById,
+    updatePostById,
+    createNewPost
 }

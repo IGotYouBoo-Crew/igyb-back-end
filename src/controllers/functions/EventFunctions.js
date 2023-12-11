756ecdc590141cdcd99de48bb83ae0a72a8679c1
Jack Dixon <126362891+JackDixon019@users.noreply.github.com>
2023-12-11 14:00:43 +1100

    Feat: added CREATE, UPDATE, and DELETE routes to event skeleton (#4)

--- src/controllers/functions/EventFunctions.js
+++ src/controllers/functions/EventFunctions.js
@@ -2,18 +2,44 @@
 // create functionality involving them.
 const { Event } = require('../../models/EventModel');
 
+// CREATE
+async function createNewEvent(data){
+    return await Event.create(data).catch((error) => error)
+}
+
+// READ
 // Model.find({}) returns all documents in a collection.
 async function getAllEvents(){
     return await Event.find({}).populate("author", "username");
+    
 }
 
-async function getEventById(EventId){
-    // finds one user with matching username
-    return await Event.findOne({id: EventId}).exec()
+async function getEventByTitle(eventTitle){
+    // finds one event with matching eventTitle
+    return await Event.findOne({title: eventTitle}).exec()
+}
+
+async function getEventById(eventId){
+    // finds event with matching eventId
+    return await Event.findById(eventId).exec()
+}
+
+// UPDATE
+async function updateEventById(eventId, updatedEventData){
+    return await Event.findByIdAndUpdate(eventId, updatedEventData, { runValidators: true, returnDocument: 'after' }).exec()
+}
+
+// DELETE
+async function deleteEventById(eventId){
+    return await Event.findByIdAndDelete(eventId).exec()
 }
 
 // Export the functions for our routes to use.
 module.exports = {
     getAllEvents,
-    getEventById
+    getEventByTitle,
+    getEventById,
+    deleteEventById,
+    updateEventById,
+    createNewEvent
 }

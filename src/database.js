679de52fcf0ad6def35800124852730c7446c5b1
JackDixon019 <13709@coderacademy.edu.au>
2023-12-13 08:28:48 +1100

    fixed typo in datbase.js: 'ciTest' -> 'citest'

--- src/database.js
+++ src/database.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 
 let DB_URI = "";
 
+
 switch (process.env.NODE_ENV.toLowerCase()) {
     case "test":
         DB_URI = "mongodb://localhost:27017/IGotYouBoo-test";
         break;
-    case "ciTest":
+    case "citest":
         DB_URI = "mongodb://mongo:27017/IGotYouBoo-test";
         break;
     case "development":

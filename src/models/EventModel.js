9b57e777bd59497c3dde8d38cb2a89e76c7d1a71
Naomi Mowbray <126124875+captainaomi@users.noreply.github.com>
2023-12-15 14:52:26 +1000

    Added further event fields, and trialling 'time' as strings (#17)
    * fix: added localhost to corsOptions
    
    * fix: added more urls to CORS
    
    * fix: added preflight as per Alex suggestion
    
    * added event fields
    
    * fix: url is not a valid mongoose schema type
    
    * updated seed.js to reflect event fields
    
    * updated server.test for correct/required fields
    
    * server.test.js update second round

--- src/models/EventModel.js
+++ src/models/EventModel.js
@@ -1,10 +1,35 @@
+const { Timestamp } = require("mongodb");
 const mongoose = require("mongoose");
 
 const EventSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    content: {type: String, required: true},
-    date: {type: String, required: true},
+    host: {type: String, required: true},
     image: {type: String, required: false},
+    title: {type: String, required: true},
+    date: {type: String, required: true},
+    start: {type: String,
+        validate: {
+        validator: function (v) {
+            if (!v) {
+            return true;
+            }
+            return /([01]?[0-9]|2[0-3]):[0-5][0-9]/.test(v);
+        },
+        message: (props) => `${props.value} is not valid time!`,
+        },
+        required: true}, 
+    finish: {type: String,
+        validate: {
+        validator: function (v) {
+            if (!v) {
+            return true;
+            }
+            return /([01]?[0-9]|2[0-3]):[0-5][0-9]/.test(v);
+        },
+        message: (props) => `${props.value} is not valid time!`,
+        },
+        required: true}, 
+    ticketLink: {type: String, required: false},
+    content: {type: String, required: true},
     author: {type: mongoose.Types.ObjectId, ref: 'User'}
 });
 

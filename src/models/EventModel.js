9e559a6921ff95b96e8009ea34355204826cc599
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-12-15 13:57:58 +1000

    added event fields

--- src/models/EventModel.js
+++ src/models/EventModel.js
@@ -1,10 +1,35 @@
+const { Timestamp } = require("mongodb");
 const mongoose = require("mongoose");
 
 const EventSchema = new mongoose.Schema({
+    host: {type: String, required: true},
+    image: {type: URL, required: false},
     title: {type: String, required: true},
-    content: {type: String, required: true},
     date: {type: String, required: true},
-    image: {type: String, required: false},
+    start: {type: String,
+        validate: {
+        validator: function (v) {
+            if (!v) {
+            return true;
+            }
+            return /([01]?[0-9]|2[0-3]):[0-5][0-9]/.test(v);
+        },
+        message: (props) => `${props.value} is not valid time!`,
+        },
+        required: true}, 
+    finish: {type: String,
+        validate: {
+        validator: function (v) {
+            if (!v) {
+            return true;
+            }
+            return /([01]?[0-9]|2[0-3]):[0-5][0-9]/.test(v);
+        },
+        message: (props) => `${props.value} is not valid time!`,
+        },
+        required: true}, 
+    ticketLink: {type: URL, required: false},
+    content: {type: String, required: true},
     author: {type: mongoose.Types.ObjectId, ref: 'User'}
 });
 

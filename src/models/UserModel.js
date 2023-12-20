f8299245a00b17df63f354b1635b8bde151e8b31
Jack Dixon <126362891+JackDixon019@users.noreply.github.com>
2023-12-21 02:06:07 +1100

    User profile picture (#37)
    * Feat: added profile picutre
    
    * Fix: update user profile now hashes string

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     pronouns: String,
-    // name: String
-    // profilePicture:
+    profilePicture: {
+        type: String,
+        required: true,
+        default:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrb5zM9IM0Calt0JRegObDpvq61W6wZ2BdGAQ1dF-i_g&s",
+    },
     role: {
         type: mongoose.Types.ObjectId,
         ref: "Role",

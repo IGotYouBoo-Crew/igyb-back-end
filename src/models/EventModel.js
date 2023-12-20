ff434cf203252d62512892e46b5fc163112edc49
captainaomi <126124875+captainaomi@users.noreply.github.com>
2023-12-20 15:59:18 +1000

    fix: formatting dates should be correct

--- src/models/EventModel.js
+++ src/models/EventModel.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const dayjs = require('dayjs');
+
 
 const EventSchema = new mongoose.Schema({
     host: {type: String, required: true},
     image: {type: String, required: false},
     title: {type: String, required: true},
-    date: {type: String, required: true},
+    date: {type: Date, required: true},
     start: {type: String,
         validate: {
         validator: function (v) {
@@ -30,6 +32,20 @@ const EventSchema = new mongoose.Schema({
     ticketLink: {type: String, required: false},
     content: {type: String, required: true},
     author: {type: mongoose.Types.ObjectId, ref: 'User'}
+});
+
+// Middleware to format the date before saving
+EventSchema.pre('save', function (next) {
+    // No need to format the date before saving, Mongoose will handle it
+    next();
+});
+
+// Getter to format the date when retrieving the document
+EventSchema.set('toObject', { getters: true });
+EventSchema.set('toJSON', { getters: true });
+
+EventSchema.path('date').get(function (value) {
+    return dayjs(value).format('DD-MM-YYYY');
 });
 
 const Event = mongoose.model("Event", EventSchema);
